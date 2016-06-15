d26554f48925cc55109936bc192f180142ec6fab
Ryan Baarz <rbaarz@adobe.com>
2016-06-15 16:46:20 -0600

    typo fix

--- purpable/public/js/rjs_config/require_setup.js
+++ purpable/public/js/rjs_config/require_setup.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", function () {
     var d = window.document,
         src = "../js/bower_components/requirejs/require.js",
         script = d.createElement('script');
-    script.setAttribute("data-main", "../js/rjs_config/req_conf.js");
+    script.setAttribute("data-main", "../js/rjs_config/require_conf.js");
     script.src = src;
     d.body.appendChild(script);
 });
