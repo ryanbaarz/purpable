5065a72482b1d9eecd12d420a3e1d8ffa60fe81b
Ryan Baarz <rbaarz@adobe.com>
2016-06-16 13:58:44 -0600

    Test

--- purpable/public/js/modules/Layout/LayoutView.js
+++ purpable/public/js/modules/Layout/LayoutView.js
@@ -20,7 +20,7 @@ define([
             box : "#rybox"
         },
         events : {
-            'click @ui.box' : '_rescale',
+            'mouseover @ui.box' : '_rescale',
         },
         initialize : function(){
 
@@ -28,18 +28,18 @@ define([
         onShow : function(){
             console.log("**********************************");
             
-            var box = document.querySelector('a-box');
+            /*var box = document.querySelector('a-box');
             console.log(box);
             console.log(this.ui.box);
             console.log(this.ui.box[0]);
-            this.ui.box.on("mouseenter", function(){
+            this.ui.box.mouseenter(function(){
                 console.log("hello");
                 box.setAttribute('scale', {
                     x: 4,
                     y: 1,
                     z: 6
                 });
-            });
+            });*/
             console.log("**********************************");
         },
         _rescale : function(e){
